736176b6a65eff2ece9fd81328eb9d013a76aa23
Filipe Catraia <filipe@flowchase.app>
2024-03-03 20:50:04 +0100

    publish

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,10 +1,27 @@
-import { GetBacon } from './utils';
+const HIDDEN_CLASS = 'visually-hidden'
 
-const baconEl = document.querySelector('.bacon');
+let cards = document.querySelectorAll('.js-search')
 
-GetBacon()
-  .then(res => {
-    const markup = res.reduce((acc, val) => (acc += `<p>${val}</p>`), '');
-    baconEl.innerHTML = markup;
-  })
-  .catch(err => (baconEl.innerHTML = err));
+function liveSearch() {
+    let search_query = document.getElementById('searchbox').value;
+
+    // Use innerText if all contents are visible
+    // Use textContent for including hidden elements
+    for (var i = 0; i < cards.length; i++) {
+        if (cards[i].textContent.toLowerCase().includes(search_query.toLowerCase())) {
+            cards[i].classList.remove(HIDDEN_CLASS);
+        } else {
+            cards[i].classList.add(HIDDEN_CLASS);
+        }
+    }
+}
+
+// A little delay
+let typingTimer;
+let typeInterval = 500;
+let searchInput = document.getElementById('searchbox');
+
+searchInput.addEventListener('keyup', () => {
+    clearTimeout(typingTimer);
+    typingTimer = setTimeout(liveSearch, typeInterval);
+});
